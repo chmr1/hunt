fa527d86d9abbe47b7b3bd5988e036dcad4c1a4f
chmr1 <chmr1@hotmail.com>
2019-05-28 00:18:04 -0300

    Implementação da listagem dos produtos

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
-//import api from '../services/api';
-import axios from 'axios'
+import api from '../services/api';
 import styles from "./style";
 
 export default class Main extends Component {
@@ -12,38 +11,58 @@ export default class Main extends Component {
     };
 
     state = {
-        docs: []
+        productInfo: {},
+        docs: [],
+        page: 1,
     };
 
-    //componentDidMount() {
-        //this.loadProducts();
-        //console.log('teste')
-    //}
-
-    loadProducts = async () => {
-
-        const productUrl = 'https://rocketseat-node.herokuapp.com/api/products';
-
-        //const response = await api.get('/products');
-        //const { docs } = response.data;
-        //this.setState({ docs });
-
-        //axios.get('https://rocketseat-node.herokuapp.com/api/products')
-        //const { docs } = response.data;
-        //this.setState({ docs });
-
-        // fetch(productUrl).then(res => res.json())
-        //     .then((data) => {
-        //         this.setState({ docs: data })
-        //         console.log(this.state.todos)
-        // }).catch(console.log)
-
+    componentDidMount() {
+        this.loadProducts();
     }
+
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const { docs, ...productInfo } = response.data;
+        this.setState({
+            docs: [...this.state.docs, ...docs ],
+            productInfo,
+            page
+        });
+    }
+
+    loadMore = () => {
+        const { page, productInfo } = this.state; // pesquisar desestruturação
+        if (page == productInfo.pages) return;
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    };
+
+    renderItem = ({ item }) => (
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.title}</Text>
+            <Text style={styles.productDescription}>{item.description}</Text>
+            <TouchableOpacity
+                style={styles.productButton}
+                onPress={() => {
+                    this.props.navigation.navigate('Product', { product: item });
+                }}
+            >
+                <Text style={styles.productButtonText}>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     render() {
         return(
             <View style={styles.container}>
-                <Text style={styles.welcome}>Página Main: Teste</Text>
+                <FlatList
+                    contentContainerStyle={styles.list}
+                    data={this.state.docs}
+                    keyExtractor={item => item._id}
+                    renderItem={this.renderItem}
+                    onEndReached={this.loadMore}
+                    onEndReachedThreshold={0.1}
+                />
             </View>
         );
     }
